a02bf2c7efdf2b2e4fd832b2f4cad7f43a31728a
Didier Alejandro Fajardo Annear <didierfajardo28@hotmail.com>
2021-09-26 13:23:58 -0500

    Delete lab1 final directory

--- lab1 final/lab1/server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-var server = express();
-server.use(express.static(__dirname+'/public'))
-server.listen(80);
--- lab1 final/lab2/db/init.js
+++
@@ -1,9 +0,0 @@
-conn = new Mongo();
-db = conn.getDB("records");
-db.createUser(
-    {
-      user: "didier",
-      pwd: "lanegra",
-      roles: [ { role: "readWrite", db: "records" } ]
-    }
-);
